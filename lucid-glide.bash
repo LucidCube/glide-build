d8eedb077bc75616e29b28ae435c806909dc3930
richard.gooding <r@jdi.io>
2017-05-25 16:29:50 +0100

    Now skips public keys

--- lucid-glide.bash
+++ lucid-glide.bash
@@ -9,14 +9,17 @@ then
     cd $KEYS_DIR
     for KEY in *
     do
-        REPO="$(echo "$KEY" | sed 's:_:/:g')"
-
-        if [ "$REPO" != "" ]
+        if [[ ! $KEY =~ \.pub$ ]]
         then
-            cat >>$GITCONFIG <<EOF
+            REPO="$(echo "$KEY" | sed 's:_:/:g')"
+
+            if [ "$REPO" != "" ]
+            then
+                cat >>$GITCONFIG <<EOF
 [url "git@github.com:$REPO"]
     insteadOf = https://github.com/$REPO
 EOF
+            fi
         fi
     done
 
