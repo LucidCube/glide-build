950c013dd86d215f89b03477719df1fa4271e0a7
richard.gooding <r@jdi.io>
2017-05-25 16:47:07 +0100

    Debug output now goes to stderr

--- ssh.sh
+++ ssh.sh
@@ -10,11 +10,11 @@ REPO_PATH=$(echo "$SSH_CMD" | sed "s/^[^ ]* '\([^']*\)'.*$/\1/")
 SHORT_REPO=$(echo "$REPO_PATH" | sed 's/\.git$//; s:/:_:g')
 KEY_FILE="$KEYS_DIR/$SHORT_REPO"
 
-echo "GIT SSH wrapper: Repo $REPO_PATH"
+echo "GIT SSH wrapper: Repo $REPO_PATH" >&2
 
 if [ "$SSH_HOST" = "git@github.com" -a -f "$KEY_FILE" ]
 then
-    echo "GIT SSH Wrapper: Using key $KEY_FILE for repo $REPO_PATH"
+    echo "GIT SSH Wrapper: Using key $KEY_FILE for repo $REPO_PATH" >&2
 	SSH_OPTS="-o IdentityFile=$KEY_FILE"
 fi
 
