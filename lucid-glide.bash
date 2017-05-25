6de70f23083f2d6f7281601a19a07840c21588ba
richard.gooding <r@jdi.io>
2017-05-25 16:19:35 +0100

    Now checks if the keys dir exists

--- lucid-glide.bash
+++ lucid-glide.bash
@@ -4,22 +4,24 @@ KEYS_DIR=/workspace/github-keys
 
 GITCONFIG=~/.gitconfig
 
-cd $KEYS_DIR
-for KEY in *
-do
-    REPO="$(echo "$KEY" | sed 's:_:/:g')"
+if [ -d $KEYS_DIR ]
+then
+    cd $KEYS_DIR
+    for KEY in *
+    do
+        REPO="$(echo "$KEY" | sed 's:_:/:g')"
 
-    if [ "$REPO" != "" ]
-    then
-        cat >>$GITCONFIG <<EOF
-
+        if [ "$REPO" != "" ]
+        then
+            cat >>$GITCONFIG <<EOF
 [url "git@github.com:$REPO"]
     insteadOf = https://github.com/$REPO
 EOF
-    fi
-done
+        fi
+    done
 
-cd - >/dev/null
+    cd - >/dev/null
+fi
 
 echo
 echo "Generated git config:"
