377a64e8c96be98050c1a3ec59db446d238def08
richard.gooding <r@jdi.io>
2017-05-25 16:32:48 +0100

    Added debug output

--- ssh.sh
+++ ssh.sh
@@ -10,8 +10,11 @@ REPO_PATH=$(echo "$SSH_CMD" | sed "s/^[^ ]* '\([^']*\)'.*$/\1/")
 SHORT_REPO=$(echo "$REPO_PATH" | sed 's/\.git$//; s:/:_:g')
 KEY_FILE="$KEYS_DIR/$SHORT_REPO"
 
+echo "GIT SSH wrapper: Repo $REPO_PATH"
+
 if [ "$SSH_HOST" = "git@github.com" -a -f "$KEY_FILE" ]
 then
+    echo "GIT SSH Wrapper: Using key $KEY_FILE for repo $REPO_PATH"
 	SSH_OPTS="-o IdentityFile=$KEY_FILE"
 fi
 
